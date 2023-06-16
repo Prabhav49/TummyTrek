c31c06dafba820d76df4977e9041b7cde382062c
Niharika <niharikasharma522@gmail.com>
2023-06-17 00:30:17 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/tummy_trek/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tummy_trek
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color pink = const Color(0xFFDB4D69);
+
+}
