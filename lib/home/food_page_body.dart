8305fb20d2224aa514106189f17558ab031558d2
Niharika <niharikasharma522@gmail.com>
2023-06-22 22:58:22 +0530

    slider completed

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -51,12 +51,23 @@ class _FoodPageBodyState extends State<FoodPageBody>{
     if(index==_currPageValue.floor()){
       var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
       var currTrans = _height*(1-currScale)/2;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
     }
     else if(index==_currPageValue.floor()+1){
       var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
       var currTrans = _height*(1-currScale)/2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else if(index==_currPageValue.floor()-1){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else{
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
     }
 
     return Transform(
@@ -85,6 +96,21 @@ class _FoodPageBodyState extends State<FoodPageBody>{
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
                   color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0,5)
+                  ),
+                  BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-5,0)
+                  ),
+                  BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(5,0)
+                  )
+                ]
               ),
               child: Container(
                 padding: EdgeInsets.only(top: 15, left: 15, right: 15),
