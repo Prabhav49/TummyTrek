cf61556a5c77efa86161c3524b49df0885ee8f6c
Niharika <niharikasharma522@gmail.com>
2023-07-29 12:49:52 +0530

    Completed recommended food details page

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -2,9 +2,11 @@ import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tummy_trek/utils/colors.dart';
 import 'package:tummy_trek/utils/dimensions.dart';
 import 'package:tummy_trek/widgets/app_icon.dart';
 import 'package:tummy_trek/widgets/big_text.dart';
+import 'package:tummy_trek/widgets/expandable_text.dart';
 
 class RecommendedFoodDetail extends StatelessWidget{
   const RecommendedFoodDetail({Key? key}): super(key: key);
@@ -12,6 +14,7 @@ class RecommendedFoodDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -26,10 +29,16 @@ class RecommendedFoodDetail extends StatelessWidget{
             bottom: PreferredSize(
               preferredSize: Size.fromHeight(20),
               child: Container(
-                 color: Colors.white,
                 child: Center(child: BigText(size: Dimensions.font26, text: "Italian Side")),
                 width: double.maxFinite,
                 padding: EdgeInsets.only(top: 5, bottom: 10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(Dimensions.radius20),
+                      topRight: Radius.circular(Dimensions.radius20)
+                  )
+                ),
               ),
             ),
             pinned: true,
@@ -44,8 +53,79 @@ class RecommendedFoodDetail extends StatelessWidget{
             ),
           ),
           SliverToBoxAdapter(
-            child: Text("Yummy Tasty Pastries"),
+            child: Column(
+              children: [
+                Container(
+                  child: ExpandableText(text: "Yummy Tasty Pastries"),
+                  margin: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
+                ),
+              ],
+            ),
           )
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20*2.5,
+              right: Dimensions.width20*2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  iconSize: Dimensions.iconSize24,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.pink,
+                     icon: Icons.remove),
+                BigText(text: "\$12.88 "+" X "+" 0 ", color: Colors.black54, size: Dimensions.font26,),
+                AppIcon(
+                    iconSize: Dimensions.iconSize24,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.pink,
+                    icon: Icons.add)
+              ],
+            ),
+          ),
+          Container(
+            height: Dimensions.bottomHeightbar,
+            padding: EdgeInsets.only(top: Dimensions.height30, bottom: Dimensions.height30, left: Dimensions.width20, right: Dimensions.width20),
+            decoration: BoxDecoration(
+                color: AppColors.pink,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(Dimensions.radius20*2),
+                    topRight: Radius.circular(Dimensions.radius20*2)
+                )
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: Dimensions.height20, bottom: Dimensions.height20, left: Dimensions.width20, right: Dimensions.width20),
+
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: Colors.white
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.pink,
+                  ),
+                ),
+                Container(
+                  child: BigText(text: "\$10 | Add to cart", color: Colors.white,),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimensions.radius20),
+                    color: AppColors.pink,
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
